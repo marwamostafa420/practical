b0c82fa360be72524068c92e4f152a7dd4ac12fe
Marwa Mostafa <marwa.mostafa420@gmail.com>
2022-07-02 04:17:54 +0200

    static

--- Project/settings.py
+++ Project/settings.py
@@ -138,10 +138,9 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/',
-]
+
+    
+
 
 STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
